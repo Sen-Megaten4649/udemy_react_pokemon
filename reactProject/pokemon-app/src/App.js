0ddf5a261ad275bc15a38f5fa1744637e46d1494
Sen <ffix.3000.c@gmail.com>
2023-12-16 21:00:54 +0900

    #2 ポケモンのデータを取得する

--- reactProject/pokemon-app/src/App.js
+++ reactProject/pokemon-app/src/App.js
@@ -1,6 +1,20 @@
+import { useEffect } from 'react';
 import './App.css';
+import { getAllPokemon } from './utils/pokemon';
 
 function App() {
+  // pokemon/以降のURLを動的に取得することで、ポケモン図鑑になる
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      //全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
+
   return (<div className="App"></div>);
 }
 
